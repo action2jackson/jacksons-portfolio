5f3b689a41771c9d79289356642dabb2d2004984
Jackson <2action2jackson2@gmail.com>
2019-12-28 10:04:16 -0600

    Created home page with no styling

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect
+import datetime
+import pytz # timezone 
+import requests
+import os
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home_page():
+	return render_template('index.html')
+
+@app.route('/<name>')
+def profile(name):
+	return render_template('index.html', name=name)     
+
+if __name__ == '__main__':
+	app.run(debug=True)
